fb616fbae5bd6e531fa9aa88f28903a007374867
aswin8959w <99708785+aswin8959w@users.noreply.github.com>
2026-02-19 00:31:16 +0530

    fix: cli build and publish

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/cli.ts'],
-  format: ['esm'],
+  format: ['cjs'],
   clean: true,
-  banner: {
-    js: '#!/usr/bin/env node',
-  },
 });
